4771031cd325d6af08e02acf853c8f0cc7560df8
NashobaPallaton <gastout.camille@gmail.com>
2022-11-09 15:41:57 +0100

    symfony day one

--- crud/add.php
+++ crud/add.php
@@ -40,7 +40,7 @@ require_once('close.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="fr"> 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
